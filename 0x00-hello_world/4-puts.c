762b618f91ffa20fdcae00854e3d4080c1222b23
irededon <isofowora@yahoo.co.uk>
2022-10-13 07:20:10 -0700

    Files added

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+ */
+ int main(void)
+	int main(void)
+{
+	{
+		  puts("\"Programming is like building a multilingual puzzle");
+		  puts("\"Programming is like building a multilingual puzzle");
+		    return (0);
+		    return (0);
+	}
+}
