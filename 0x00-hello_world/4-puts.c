8da28078905c08fdf30c538f27ecfe3e49eff357
irededon <isofowora@yahoo.co.uk>
2022-10-13 11:36:53 -0700

    Adding the forth

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
- */
- int main(void)
-	int main(void)
+#include <stdio.h>
+/**
+ * * main - Entry point
+ *  *
+ *   * Return: Always 0 (Success)
+ *    */
+int main(void)
 {
-	{
-		  puts("\"Programming is like building a multilingual puzzle");
-		  puts("\"Programming is like building a multilingual puzzle");
-		    return (0);
-		    return (0);
-	}
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
